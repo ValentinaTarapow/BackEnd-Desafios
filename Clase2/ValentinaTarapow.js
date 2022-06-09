21879a2f7aebcff3dea26b9d48963f344e2c74b6
ValentinaTarapow <tarapow.v@gmail.com>
2022-06-08 23:03:35 -0300

    add resolution:  Desafio Clases (Clase 2)

---
+++ Clase2/ValentinaTarapow.js
@@ -0,0 +1,43 @@
+class cUser{
+    constructor(name, surname, books, pets){
+        this.name = name;
+        this.surname = surname;
+        this.books = books;
+        this.pets = pets;
+    }
+
+    getFullName(){
+        return `${this.name} ${this.surname}`;
+    }
+
+    addPet(petName){
+        this.pets.push(petName);
+    }
+
+    countPets(){
+        return this.pets.length;
+    }
+
+    addBook(bookName, author){
+        this.books.push({ title: bookName, author:  author});
+    }
+
+    getBookNames(){
+        const booksNames = this.books.map(function (book) {
+            return book.title
+        });
+
+        return booksNames;
+    }
+}
+
+
+const myUser = new cUser('Valentina', 'Tarapow' , [{title: 'Estudio en escarlata', author: 'Arthur Conan Doyle' }], ['Piku', 'Mikey'] );
+
+console.log(myUser.getFullName())
+console.log(myUser.countPets())
+myUser.addPet('Suki')
+console.log(myUser.countPets())
+console.log(myUser.getBookNames())
+myUser.addBook('Maus', 'Art Spiegelman')
+console.log(myUser.getBookNames())
